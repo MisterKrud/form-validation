64b7fe81e40c55316559ec8b67389938fdf7b337
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-08-21 04:31:06 +0000

    Update html with button element & IDs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 const content = document.getElementById("content")
 
 console.log('starting')
+
+const form = document.querySelector("form");
+const email = document.getElementById("email");
+const country = document.getElementById("country");
+const postcode = document.getElementById("postcode");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm-password");
+
+const emailRegExp = "/^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d-]+(?:\.[a-z\d-]+)*$/i;";
