51b9d6c271fed7f37eea895f6891e228783c2d33
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-08-21 05:04:31 +0000

    Add initial validity checkers in js

--- src/index.js
+++ src/index.js
@@ -10,3 +10,34 @@ const password = document.getElementById("password");
 const confirmPassword = document.getElementById("confirm-password");
 
 const emailRegExp = "/^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d-]+(?:\.[a-z\d-]+)*$/i;";
+const passwordRegExp = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$";
+
+const isValidEmail = () =>{
+    const validity = email.ariaValueMax.length !== 0 && emailRegExp.test(email.value);
+    return validity
+};
+
+
+const isValidCountry = () => {
+    const validity = country.value.length >=3;
+    return validity;
+}
+
+const isValidPostcode = () =>{
+    const validity = postcode.length === 4;
+    return validity;
+}
+
+const isValidPassword = () => {
+    const validity = password.value.length >= 8 && passwordRegExp.test(password.value);
+    return validity;
+}
+
+const passwordsMatch = () => {
+    const validity = confirmPassword.value === password.value;
+    return validity;
+}
+
+
+
+
