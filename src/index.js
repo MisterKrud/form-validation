f5aaf93c7e8194e3daa4118a9e8ccee017885b30
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-08-22 21:02:14 +1000

    Add email validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { is } from "date-fns/locale";
 import  "./styles.css"
 
 const content = document.getElementById("content")
@@ -14,14 +15,49 @@ const confirmPassword = document.getElementById("confirm-password");
 const emailRegExp = "/^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d-]+(?:\.[a-z\d-]+)*$/i;";
 const passwordRegExp = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$";
 
+const emailError = document.querySelector("#email + span.error")
+
+const blankInputErrorMessage = "You didn't type anything!"
+
+// const createInputErrorText = (currentInput, inputType) => {
+//     const inputArea = currentInput.setAttribute("")
+// }
+
 const isValidEmail = () =>{
     const validity = email.ariaValueMax.length !== 0 && emailRegExp.test(email.value);
+    
     return validity
 };
 
 
+email.addEventListener("input", (event) =>{
+    email.classList.remove("blank");
+    if (!isValidEmail) {
+        console.log('email validity: '+isValidEmail.validity)
+        emailError.textContent = "valid";
+        emailError.className = "error";
+    } else {
+       showEmailError()
+       
+    }
+})
+
+const showEmailError = () =>{
+    console.log('error goes here');
+   
+    if (email.validity.valueMissing) {
+        emailError.textContent = blankInputErrorMessage
+       
+    } else if (email.validity.typeMismatch) {
+        emailError.textContent = "Umm. That's not an email. Ok?"     
+    } 
+}
+
 const isValidCountry = () => {
     const validity = country.value.length >=3;
+    if (validity) {
+        country.classList.add("validated");
+    }
     return validity;
 }
 
@@ -35,11 +71,25 @@ const isValidPassword = () => {
     return validity;
 }
 
+password.addEventListener("input", (event) => {
+    password.classList.remove("blank");
+    if (!isValidPassword) {
+
+    }
+})
+
 const passwordsMatch = () => {
     const validity = confirmPassword.value === password.value;
     return validity;
 }
 
+form.addEventListener("submit" , (event) => {
+    isValidPassword();
+    if (!email.validity.valid || !password.classList.contains("validated")) {
+      
+        event.preventDefault();
+    }
+})
 
 
 
