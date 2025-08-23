7d2d435335ad620459bfea7f5576936eb310f5c1
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-08-23 15:18:45 +1000

    Create generic validation function

--- src/index.js
+++ src/index.js
@@ -7,57 +7,90 @@ console.log('starting')
 
 const form = document.querySelector("form");
 const email = document.getElementById("email");
-const country = document.getElementById("country");
+const state = document.getElementById("state");
 const postcode = document.getElementById("postcode");
 const password = document.getElementById("password");
 const confirmPassword = document.getElementById("confirm-password");
 
-const emailRegExp = "/^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d-]+(?:\.[a-z\d-]+)*$/i;";
-const passwordRegExp = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$";
+const emailRegExp = /^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d-]+(?:\.[a-z\d-]+)*$/i;
+const passwordRegExp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+const ausStates = ['NSW', 'QLD', 'SA', 'VIC', 'WA', 'NT', 'ACT', 'TAS']
 
-const emailError = document.querySelector("#email + span.error")
+// const emailError = document.querySelector("#email + span.error")
 
 const blankInputErrorMessage = "You didn't type anything!"
+
+
+const inputs = document.querySelectorAll("input");
 
 // const createInputErrorText = (currentInput, inputType) => {
 //     const inputArea = currentInput.setAttribute("")
 // }
 
-const isValidEmail = () =>{
-    const validity = email.ariaValueMax.length !== 0 && emailRegExp.test(email.value);
+// const isValidEmail = () =>{
+//     const validity = email.value.length !== 0 && emailRegExp.test(email.value);
     
-    return validity
-};
+//     return validity
+// };
+
+const emailRule = "must be a valid email address"
 
 
-email.addEventListener("input", (event) =>{
-    email.classList.remove("blank");
-    if (!isValidEmail) {
-        console.log('email validity: '+isValidEmail.validity)
-        emailError.textContent = "valid";
-        emailError.className = "error";
-    } else {
-       showEmailError()
-       
-    }
+inputs.forEach(inp => {
+    inp.addEventListener("input", (event) => {
+        inp.classList.remove("blank");
+        const parent = inp.parentElement;
+        const errorField = parent.querySelector("span");
+        errorField.classList.add("active");
+        if(inp.validity.valid) {
+            errorField.textContent = "";
+            errorField.className = "valid-field";
+        } else {
+            showError(inp, errorField)
+        }
+        
+    })
 })
 
-const showEmailError = () =>{
-    console.log('error goes here');
-   
-    if (email.validity.valueMissing) {
-        emailError.textContent = blankInputErrorMessage
+
+// email.addEventListener("input", (event) =>{
+//     email.classList.remove("blank");
+//      emailError.classList.add("active");
+//     if (email.validity.valid) {
+//         console.log('email validity: '+isValidEmail.validity)
+//         emailError.textContent = "";
+//         emailError.className = "valid-field";
+//     } else {
        
-    } else if (email.validity.typeMismatch) {
-        emailError.textContent = "Umm. That's not an email. Ok?"     
-    } 
+//        showError(email, emailError)
+       
+//     }
+// })
+
+const showError = (input, errorField, errorMessage) =>{
+    console.log('error goes here');
+    const inputParent = input.parentElement;
+    const inputTitle = inputParent.querySelector("p");
+    const inputName = input.getAttribute("name");
+    errorField.classList.add("error");
+    if (input.validity.valueMissing) {
+        errorField.textContent = `This field cannot be empty`
+    } else if (inputName === "password") {
+        errorField.textContent = `Your password must be at least 8 characters`
+    
+       
+    } else if (input.validity.typeMismatch) {
+        errorField.textContent = `${inputTitle} must be a valid ${inputName}`     
+    } else {
+        errorField.textContent = `Problem with ${input.getAttribute("name")}`
+    }
 }
 
-const isValidCountry = () => {
-    const validity = country.value.length >=3;
-    if (validity) {
-        country.classList.add("validated");
-    }
+const isValidState = () => {
+    const validity = state.value.length >=3;
+   
+     
+    
     return validity;
 }
 
@@ -72,8 +105,8 @@ const isValidPassword = () => {
 }
 
 password.addEventListener("input", (event) => {
-    password.classList.remove("blank");
-    if (!isValidPassword) {
+    // password.classList.remove("blank");
+    if (isValidPassword) {
 
     }
 })
